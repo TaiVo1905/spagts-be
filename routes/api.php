e31155aa3f860f50e604fcab40d503e1682725cc
Võ Thị Thu Hiền <hien.vo26@student.passerellesnumeriques.org>
2025-05-09 22:05:53 +0700

    SPGTS-24, SPGTS-47: Done feature to create a new class

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,12 @@
 <?php
 use App\Http\Controllers\Api\V1\UserController;
+use App\Http\Controllers\Api\V1\ClassController;
+
 
 Route::prefix('v1')->group(function () {
     Route::apiResource('users', UserController::class);
+});
+
+Route::prefix('v1')->group(function () {
+    Route::apiResource('class-names', ClassController::class);
 });
