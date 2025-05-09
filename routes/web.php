e3467892fe090766965d5de6fb49b1fa96f75110
unknown <voductaitxqt123@mail.com>
2025-05-10 05:09:05 +0700

    SPGTS-52 api: Done api for user roles

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,34 @@
 <?php
-
+use App\Services\Clouds\CloudinaryService;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/test-cloudinary', function() {
+    try {
+        $cloudinary = new CloudinaryService([
+            'cloud' => [
+                'cloud_name' => env('CLOUDINARY_CLOUD_NAME'),
+                'api_key' => env('CLOUDINARY_API_KEY'),
+                'api_secret' => env('CLOUDINARY_API_SECRET'),
+            ],
+            'url' => [
+                'secure' => true
+            ]
+        ]);
+        return response()->json(['status' => 'success']);
+    } catch (Exception $e) {
+        return response()->json([
+            'status' => 'error',
+            'message' => $e->getMessage(),
+            'env' => [
+                'cloud_name' => env('CLOUDINARY_CLOUD_NAME'),
+                'api_key' => env('CLOUDINARY_API_KEY'),
+                'api_secret' => env('CLOUDINARY_API_SECRET'),
+                'url' => env('CLOUDINARY_URL')
+            ]
+        ], 500);
+    }
 });
